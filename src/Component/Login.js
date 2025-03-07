efd1a4841f1ae5a0bafa949d3c8c820826401351
PARKSEONAH251 <161944840+PARKSEONAH251@users.noreply.github.com>
2025-03-07 22:07:18 +0900

    backend 최신 업데이트

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -16,24 +16,27 @@ export default function Login() {
     };
 
     try {
-      const response = await fetch("http://localhost:8080/login", {
+      const response = await fetch("http://localhost:8080/request/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify(userInfo),
       });
-
+      
       if (response.ok) {
         console.log("Login successful");
-        sessionStorage.setItem("userid", userid);
-        navigate("/main");
-        // Success logic (e.g., redirect)
+        sessionStorage.setItem("userid", userid); // 로그인한 사용자 정보 저장
+        alert("로그인 성공!");
+        navigate("/main"); //메인 페이지 이동
+        // 성공 시 추가적인 로직 (예: 리다이렉트)
       } else {
+        alert("로그인 실패! 아이디 또는 비밀번호를 확인하세요.");
         console.error("Invalid credentials");
-        // Failure logic
+        // 실패 시 추가적인 로직
       }
     } catch (error) {
+      alert("서버 오류 발생! 관리자에게 문의하세요.");
       console.error("Error:", error);
     }
   };
